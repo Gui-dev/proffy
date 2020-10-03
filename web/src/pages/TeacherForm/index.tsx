86f368ea385ebc08ccdd18ffca9d5c41d82252bc
Gui-dev <adrianon10@gmail.com>
2020-10-03 02:14:42 -0300

    feat: create page TeacherForm

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './style'
+
+export const TeacherForm: React.FC = () => {
+  return (
+    <Container>
+      Teacher Form
+    </Container>
+  )
+}
