920c4b2935c90192b77b2fbbd65e4863154bf092
Gui-dev <adrianon10@gmail.com>
2020-10-03 02:31:35 -0300

    refactor: add new route /give-classes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Landing } from './../pages/Landing'
 import { TeacherList } from './../pages/TeacherList'
+import { TeacherForm } from './../pages/TeacherForm'
 
 export const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={ Landing }/>
-        <Route path="/teachers" exact component={ TeacherList }/>
+        <Route path="/study" exact component={ TeacherList }/>
+        <Route path="/give-classes" exact component={ TeacherForm }/>
       </Switch>
     </BrowserRouter>
   )
