a9fd8c67eb34f742f51884c3d9356e1c5ec52fe2
Gui-dev <adrianon10@gmail.com>
2020-10-03 02:14:23 -0300

    feat: create page TeacherList

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './style'
+
+export const TeacherList: React.FC = () => {
+  return (
+    <Container>
+      Teacher
+    </Container>
+  )
+}
