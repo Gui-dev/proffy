46951918f8859d39069844b4f2821758a64842c1
Gui-dev <adrianon10@gmail.com>
2020-10-04 01:53:40 -0300

    refactor: add component PageHeader

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import { PageHeader } from './../../components/PageHeader'
 import { Container } from './style'
 
 export const TeacherForm: React.FC = () => {
   return (
-    <Container>
-      Teacher Form
+    <Container className="container">
+      <PageHeader title="Que incrível que você quer dar aulas"/>
     </Container>
   )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import { PageHeader } from './../../components/PageHeader'
 import { Container } from './style'
 
 export const TeacherList: React.FC = () => {
   return (
-    <Container>
-      Teacher
+    <Container className="container">
+      <PageHeader title="Estes são os proffys disponíveis."/>
     </Container>
   )
 }
