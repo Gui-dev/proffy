327ba17a84b36aedc12412c71dd5e5b97a6d3ca0
Gui-dev <adrianon10@gmail.com>
2020-10-06 02:31:31 -0300

    refactor: add form and style reponsive

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 import { PageHeader } from './../../components/PageHeader'
-import { Container } from './style'
+import { Container, Form, InputBlock } from './style'
 
 export const TeacherList: React.FC = () => {
   return (
     <Container className="container">
-      <PageHeader title="Estes são os proffys disponíveis."/>
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <Form>
+          <InputBlock>
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" className="subject"/>
+          </InputBlock>
+
+          <InputBlock>
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week-day" className="week-day"/>
+          </InputBlock>
+
+          <InputBlock>
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" className="time"/>
+          </InputBlock>
+        </Form>
+      </PageHeader>
     </Container>
   )
 }
