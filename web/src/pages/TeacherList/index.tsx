8f7ce63f631a84fcb02fddfde1e7d80edf47e992
Gui-dev <adrianon10@gmail.com>
2020-10-10 02:56:32 -0300

    refactor: add component Input

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,27 +2,17 @@ import React from 'react'
 
 import { PageHeader } from './../../components/PageHeader'
 import { TeacherItem } from './../../components/TeacherItem'
-import { Container, Form, InputBlock, Main } from './style'
+import { Input } from './../../components/Input'
+import { Container, Form, Main } from './style'
 
 export const TeacherList: React.FC = () => {
   return (
     <Container className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <Form>
-          <InputBlock>
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" className="subject"/>
-          </InputBlock>
-
-          <InputBlock>
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" className="week-day"/>
-          </InputBlock>
-
-          <InputBlock>
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" className="time"/>
-          </InputBlock>
+          <Input name="subject" label="Matéria"/>
+          <Input name="week-day" label="Dia da semana"/>
+          <Input type="time" name="time" label="Hora"/>
         </Form>
       </PageHeader>
 
--- web/src/pages/TeacherList/style.ts
+++ web/src/pages/TeacherList/style.ts
@@ -21,49 +21,6 @@ export const Form = styled.form`
   }
 `
 
-export const InputBlock = styled.div`
-  position: relative;
-
-  & + div {
-    margin-top: 1.4rem;
-  }
-
-  @media (min-width: 700px) {
-    & + div {
-      margin-top: 0;
-    }
-  }
-
-  &:focus-within::after {
-    content: '';
-    position: absolute;
-    left: 1.6rem;
-    right: 1.6rem;
-    bottom: 0;
-    width: calc(100% - 3.2rem);
-    height: 2px;
-    background-color: var(--color-primary-light);
-  }
-
-  label {
-    font-size: 1.4rem;
-    color: var(--color-text-in-primary);
-  }
-
-  input {
-    font-size: 1.6rem;
-    font-family: Archivo;
-    width: 100%;
-    height: 5.6rem;
-    margin-top: 0.8rem;
-    padding: 0 1.6rem;
-    background-color: var(--color-input-background);
-    border-radius: 0.8rem;
-    border: 1px solid var(--color-line-in-white);
-    outline: 0;
-  }
-`
-
 export const Main = styled.main`
   width: 90%;
   margin: 3.2rem auto;
