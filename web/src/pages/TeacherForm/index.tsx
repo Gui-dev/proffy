39daa2f33c7870b96ea17d0797fa1e86f484f7e6
Gui-dev <adrianon10@gmail.com>
2020-10-13 02:34:42 -0300

    refactor: add component Textarea

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
 
 import { PageHeader } from './../../components/PageHeader'
-import { Container } from './style'
+import { Input } from './../../components/Input'
+import { Textarea } from '../../components/Textarea'
+import warningIcon from './../../assets/images/icons/warning.svg'
+import { Container, Main, Form } from './style'
 
 export const TeacherForm: React.FC = () => {
   return (
     <Container className="container">
-      <PageHeader title="Que incrível que você quer dar aulas"/>
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher o formulário de inscrição"
+      />
+
+      <Main>
+        <Form>
+
+          <fieldset>
+            <legend>Seus dados</legend>
+            <Input name="name" label="Nome completo"/>
+            <Input name="avatar" label="Avatar"/>
+            <Input name="whatsapp" label="Whatsapp"/>
+            <Textarea name="bio" label="Biográfia"/>
+          </fieldset>
+
+          <fieldset>
+            <legend>Sobre a aula</legend>
+            <Input name="subject" label="Matéria"/>
+            <Input name="cost" label="Custo da sua hora por aula"/>
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={ warningIcon } alt="Aviso importante" title="Aviso importante"/>
+              Importante!<br/>
+              Preencha todos os dados
+            </p>
+            <button>Salvar cadastro</button>
+          </footer>
+
+        </Form>
+      </Main>
     </Container>
   )
 }
--- web/src/pages/TeacherForm/style.ts
+++ web/src/pages/TeacherForm/style.ts
@@ -1,6 +1,118 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100vw !important;
+  width: 100vw;
   height: 100vh;
+
+  @media (min-width: 700px) {
+    width: 100vw;
+  }
+`
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: 74rem;
+  padding: 6.4rem;
+  margin: -3.2rem auto 3.2rem;
+  background-color: var(--color-box-base);
+  border-radius: 0.8rem;
+  overflow: hidden;
+`
+
+export const Form = styled.form`
+  max-width: 100%;
+
+  fieldset {
+    max-width: 100%;
+    padding: 0 2.4rem;
+    border: 0;
+
+    & + fieldset {
+      margin-top: 6.4rem;
+    }
+
+    & + textarea {
+      margin-top: 2.4rem;
+    }
+
+    @media (min-width: 700px) {
+      padding: 0 6.4rem;
+    }
+
+    label {
+      color: var(--color-text-complement);
+    }
+  }
+
+  legend {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 2.4rem;
+    font-weight: 700;
+    font-family: Archivo;
+    color: var(--color-text-title);
+    width: 100%;
+    padding-bottom: 1.6rem;
+    margin-bottom: 2.4rem;
+    border-bottom: 1px solid var(--color-line-in-white);
+  }
+
+  footer {
+    width: 100%;
+    padding: 4rem 2.4rem;
+    margin-top: 6.4rem;
+    background-color: var(--color-box-footer);
+    border-top: 1px solid var(--color-line-in-white);
+
+    p {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.4rem;
+      line-height: 2.4rem;
+      color: var(--color-text-complement);
+
+      img {
+        margin-right: 1rem;
+      }
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.6rem;
+      font-family: Archivo;
+      color: var(--color-button-text);
+      width: 100%;
+      height: 5.6rem;
+      margin-top: 3.2rem;
+      background-color: var(--color-secundary);
+      border: 0;
+      border-radius: 0.8rem;
+      transition: background-color 0.2s;
+      cursor: pointer;
+
+      &:hover {
+        background-color: var(--color-secundary-dark);
+      }
+    }
+
+    @media (min-width: 700px) {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 4rem 6.4rem;
+
+      p {
+        justify-content: space-between;
+      }
+
+      button {
+        width: 20rem;
+        margin-top: 0;
+      }
+    }
+  }
 `
