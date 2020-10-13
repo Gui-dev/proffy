5c4de3f7b706df12f63f2c2d4afdbff049b441a6
Gui-dev <adrianon10@gmail.com>
2020-10-13 03:20:54 -0300

    feat: create component Select

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,30 @@
+import React, { SelectHTMLAttributes } from 'react'
+
+import { SelectBlock } from './style'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string
+  label: string
+  options: Array<{
+    value: string
+    label: string
+  }>
+}
+
+export const Select: React.FC<SelectProps> = ({ children, name, label, options, ...rest }) => {
+  return (
+    <SelectBlock>
+      <label htmlFor={ name }>{ label }</label>
+      <select { ...rest } id={ name }>
+        <option value="DEFAULT" disabled selected hidden>Selecione uma opção</option>
+        { options.map( option => {
+          return (
+            <option key={ option.value } value={ option.value }>
+              { option.label }
+            </option>
+          )
+        } ) }
+      </select>
+    </SelectBlock>
+  )
+}
---
+++ web/src/components/Select/style.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const SelectBlock = styled.div`
+  position: relative;
+
+  & + div {
+    margin: 1.4rem 0;
+  }
+
+  @media (min-width: 700px) {
+    & + div {
+      margin: 1.4rem 0;
+    }
+  }
+
+  &:focus-within::after {
+    content: '';
+    position: absolute;
+    left: 1.6rem;
+    right: 1.6rem;
+    bottom: 0;
+    width: calc(100% - 3.2rem);
+    height: 2px;
+    background-color: var(--color-primary-light);
+  }
+
+  label {
+    font-size: 1.4rem;
+    color: var(--color-text-in-primary);
+  }
+
+  select {
+    font-size: 1.6rem;
+    font-family: Archivo;
+    color: var(--color-text-complement);
+    width: 100%;
+    height: 5.6rem;
+    margin-top: 0.8rem;
+    padding: 0 1.6rem;
+    background-color: var(--color-input-background);
+    border-radius: 0.8rem;
+    border: 1px solid var(--color-line-in-white);
+    outline: 0;
+  }
+`
