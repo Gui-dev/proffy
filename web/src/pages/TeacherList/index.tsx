8f1791199deeb7db207f7c358338b2afe364d752
Gui-dev <adrianon10@gmail.com>
2020-10-13 03:21:35 -0300

    refactor: add component Select

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { PageHeader } from './../../components/PageHeader'
 import { TeacherItem } from './../../components/TeacherItem'
 import { Input } from './../../components/Input'
+import { Select } from '../../components/Select'
 import { Container, Form, Main } from './style'
 
 export const TeacherList: React.FC = () => {
@@ -10,8 +11,35 @@ export const TeacherList: React.FC = () => {
     <Container className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <Form>
-          <Input name="subject" label="Matéria"/>
-          <Input name="week-day" label="Dia da semana"/>
+          <Select
+              name="subject"
+              label="Matéria"
+              options={[
+                { value: 'Artes', label: 'Artes' },
+                { value: 'Biologia', label: 'Biologia' },
+                { value: 'Ciências', label: 'Ciências' },
+                { value: 'Educação física', label: 'Educação física' },
+                { value: 'Geografia', label: 'Geografia' },
+                { value: 'História', label: 'História' },
+                { value: 'Matemática', label: 'Matemática' },
+                { value: 'Português', label: 'Português' },
+                { value: 'Química', label: 'Química' },
+                { value: 'Inglês', label: 'Inglês' }
+              ]}
+            />
+          <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda' },
+                { value: '2', label: 'Terça' },
+                { value: '3', label: 'Quarta' },
+                { value: '4', label: 'Quinta' },
+                { value: '5', label: 'Sexta' },
+                { value: '6', label: 'Sábado' }
+              ]}
+            />
           <Input type="time" name="time" label="Hora"/>
         </Form>
       </PageHeader>
--- web/src/pages/TeacherList/style.ts
+++ web/src/pages/TeacherList/style.ts
@@ -12,12 +12,24 @@ export const Container = styled.div`
 export const Form = styled.form`
   margin-top: 3.2rem;
 
+  & div:first-child {
+    margin-top: 10rem;
+  }
+
+  & div {
+    margin-top: 0 !important;
+  }
+
   @media (min-width: 700px) {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     column-gap: 1.6rem;
     position: absolute;
     bottom: -2.8rem;
+
+    & div {
+      margin-top: 0;
+    }
   }
 `
 
