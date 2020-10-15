104c11ae33d9cc5c8191694277ecdd915aa5e6ae
Gui-dev <adrianon10@gmail.com>
2020-10-15 02:37:23 -0300

    refactor: add total connections

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import logoImg from './../../assets/images/logo.svg'
@@ -6,9 +6,21 @@ import landingImg from './../../assets/images/landing.svg'
 import studyImg from './../../assets/images/icons/study.svg'
 import giveClassesIcon from './../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from './../../assets/images/icons/purple-heart.svg'
+import { api } from '../../services/api'
 import { Container, PageContainer, LogoContainer, ButtonContainer } from './style'
 
 export const Landing: React.FC = () => {
+
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('/connections')
+       .then(response => {
+         const { total } = response.data
+         setTotalConnections(total)
+       })
+  }, [])
+
   return (
     <Container>
       <PageContainer className="container">
@@ -31,7 +43,7 @@ export const Landing: React.FC = () => {
         </ButtonContainer>
 
         <span>
-          Total de 200 conexões já realizadas
+          Total de { totalConnections } conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" title="Coração roxo"/>
         </span>
       </PageContainer>
