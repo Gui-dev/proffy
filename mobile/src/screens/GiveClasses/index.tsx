f3be0c223ac68e68622bbb26eec7c31c13940d68
Gui-dev <adrianon10@gmail.com>
2020-10-19 03:38:48 -0300

    feat: create GiveClasses page

---
+++ mobile/src/screens/GiveClasses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './style'
+
+export const GiveClasses: React.FC = () => {
+  return (
+    <Container>
+      <Text>Give Classes</Text>
+    </Container>
+  )
+}
