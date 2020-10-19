baeb4061a347adbe0c262860b68179af0d7504aa
Gui-dev <adrianon10@gmail.com>
2020-10-19 03:39:50 -0300

    refactor: add route component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { AppLoading } from 'expo'
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
-import { Landing } from './src/screens/Landing'
+import { AppStack } from './src/routes/AppStack.routes'
 
 export default function App() {
 
@@ -21,7 +21,7 @@ export default function App() {
 
   return (
     <>
-      <Landing />
+      <AppStack />
       <StatusBar style="light"/>
     </>
   )
