2947f7de31d6fffe4c749a7614d1105e75213453
Gui-dev <adrianon10@gmail.com>
2020-10-20 02:51:37 -0300

    refactor: ending page giveclasses

--- mobile/src/screens/GiveClasses/index.tsx
+++ mobile/src/screens/GiveClasses/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
-import { Container } from './style'
+import giveClassesBgImage from './../../assets/images/give-classes-background.png'
+import { Container, ImageBackground, Title, Description, OkButton, OkButtonText } from './style'
 
 export const GiveClasses: React.FC = () => {
+
+  const { goBack } = useNavigation()
+
+  const handleGoHome = () => {
+    goBack()
+  }
+
   return (
     <Container>
-      <Text>Give Classes</Text>
+      <ImageBackground
+        source={giveClassesBgImage}
+        resizeMode="contain"
+      >
+        <Title>Quer ser um Proffy?</Title>
+        <Description>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web
+        </Description>
+      </ImageBackground>
+
+      <OkButton onPress={ handleGoHome }>
+        <OkButtonText>Tudo bem</OkButtonText>
+      </OkButton>
     </Container>
   )
 }
