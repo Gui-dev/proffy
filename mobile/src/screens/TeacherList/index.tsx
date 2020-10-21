ac6a101172fcfc9059d3d29ed44744eb1b3cad7b
Gui-dev <adrianon10@gmail.com>
2020-10-21 02:21:30 -0300

    feat: create page TeacherList

---
+++ mobile/src/screens/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { PageHeader } from './../../components/PageHeader'
+import { Container } from './style'
+
+export const TeacherList: React.FC = () => {
+  return (
+    <Container>
+      <PageHeader title="Proffys disponíveis"/>
+    </Container>
+  )
+}
