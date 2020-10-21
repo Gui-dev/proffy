1aa7e2b386fdbe9675a4edaf6c5e5b59e6f4ba8d
Gui-dev <adrianon10@gmail.com>
2020-10-21 03:33:21 -0300

    refactor: add component TeacherItem

--- mobile/src/screens/Favorites/index.tsx
+++ mobile/src/screens/Favorites/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { PageHeader } from '../../components/PageHeader'
 
-import { Container } from './style'
+import { PageHeader } from '../../components/PageHeader'
+import { TeacherItem } from '../../components/TeacherItem'
+import { Container, FavoritesListItems } from './style'
 
 export const Favorites: React.FC = () => {
   return (
     <Container>
       <PageHeader title="Meus proffys favoritos"/>
+
+      <FavoritesListItems>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </FavoritesListItems>
     </Container>
   )
 }
--- mobile/src/screens/TeacherList/index.tsx
+++ mobile/src/screens/TeacherList/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { ScrollView } from 'react-native'
 
 import { PageHeader } from './../../components/PageHeader'
-import { Container } from './style'
+import { TeacherItem } from '../../components/TeacherItem'
+import { Container, TeacherListItems } from './style'
 
 export const TeacherList: React.FC = () => {
   return (
     <Container>
       <PageHeader title="Proffys disponíveis"/>
+
+      <TeacherListItems>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </TeacherListItems>
     </Container>
   )
 }
