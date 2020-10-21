fc6036f827e51916169376cc163131ac6d36b0fb
Gui-dev <adrianon10@gmail.com>
2020-10-21 03:34:40 -0300

    style: add style TeacherListItems

--- mobile/src/screens/TeacherList/style.ts
+++ mobile/src/screens/TeacherList/style.ts
@@ -4,3 +4,12 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #F0F0F7;
 `
+
+export const TeacherListItems = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingHorizontal: 16,
+    paddingBottom: 16
+  }
+})`
+  margin-top: -60px;
+`
