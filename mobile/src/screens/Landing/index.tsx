b9f0bcb7c311b14e98149ba32ad833d1ddeead94
Gui-dev <adrianon10@gmail.com>
2020-10-22 03:07:02 -0300

    refactor: add total connections

--- mobile/src/screens/Landing/index.tsx
+++ mobile/src/screens/Landing/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import landingImage from './../../assets/images/landing.png'
 import studyIcon from './../../assets/images/icons/study.png'
 import giveClassesIcon from './../../assets/images/icons/give-classes.png'
 import heartIcon from './../../assets/images/icons/heart.png'
+import { api } from '../../services/api'
 import { Container, Image, Title, SubTitle, ButtonsContainer,
   ButtonPrimary, ImageIcon, ButtonText, ButtonSecondary, TotalConnections } from './style'
 
 export const Landing: React.FC = () => {
 
   const { navigate } = useNavigation()
+  const [totalConnection, setTotalConnection] = useState(0)
 
   const handleNavigateToGiveClasses = () => {
     navigate('GiveClasses')
@@ -19,6 +21,14 @@ export const Landing: React.FC = () => {
   const handleNavigateToStudyPages = () => {
     navigate('Study')
   }
+
+  useEffect(() => {
+    api.get('/connections')
+      .then(response => {
+        const { total } = response.data
+        setTotalConnection(total)
+      })
+  }, [])
 
   return (
     <Container>
@@ -41,7 +51,7 @@ export const Landing: React.FC = () => {
       </ButtonsContainer>
 
       <TotalConnections>
-        Total de 200 conexões já realizadas {' '}
+        Total de { totalConnection } conexões já realizadas {' '}
         <ImageIcon source={ heartIcon }/>
       </TotalConnections>
     </Container>
