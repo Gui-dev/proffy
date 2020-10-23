efadaa40cccc1af3fa8b189b4f2305abb93e86a8
Gui-dev <adrianon10@gmail.com>
2020-10-23 03:07:22 -0300

    refactor: add helper method loadFavorites and useFocusEffect

--- mobile/src/screens/Favorites/index.tsx
+++ mobile/src/screens/Favorites/index.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import { PageHeader } from '../../components/PageHeader'
 import { TeacherItem } from '../../components/TeacherItem'
+import { TeacherProps } from '../../components/TeacherItem/TeacherItem.interface'
 import { Container, FavoritesListItems } from './style'
 
 export const Favorites: React.FC = () => {
+
+  const [favorites, setFavorites] = useState([])
+
+  const loadFavorites = async () => {
+    const response = await AsyncStorage.getItem('favorites')
+
+    if (response) {
+      const favoritedTeachers = JSON.parse(response)
+      setFavorites(favoritedTeachers)
+    }
+  }
+
+  useFocusEffect(() => {
+    loadFavorites()
+  })
+
   return (
     <Container>
       <PageHeader title="Meus proffys favoritos"/>
 
       <FavoritesListItems>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+
+        { favorites.map((teacher: TeacherProps) => (
+          <TeacherItem
+            key={ teacher.id }
+            teacher={ teacher }
+            favorited
+          />
+        )) }
+
       </FavoritesListItems>
     </Container>
   )
