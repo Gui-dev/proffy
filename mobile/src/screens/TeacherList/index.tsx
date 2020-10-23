d99ce5ac984ea3a24051000aae43d18d22b38560
Gui-dev <adrianon10@gmail.com>
2020-10-23 01:55:50 -0300

    refactor: add helper method handleFilterSubmit

--- mobile/src/screens/TeacherList/index.tsx
+++ mobile/src/screens/TeacherList/index.tsx
@@ -3,16 +3,35 @@ import { Feather } from '@expo/vector-icons'
 
 import { PageHeader } from './../../components/PageHeader'
 import { TeacherItem } from '../../components/TeacherItem'
+import { api } from '../../services/api'
+import { TeacherProps } from '../../components/TeacherItem/TeacherItem.interface'
 import { Container, TeacherListItems, SearchForm, Label, Input, InputGroup, InputBlock,
   FilterButton, SubmitButton, SubmitButtonText
 } from './style'
 
 export const TeacherList: React.FC = () => {
 
+  const [teachers, setTeachers] = useState([])
   const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+  const [subject, setSubject] = useState('')
+  const [week_day, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
 
   const handleToggleFiltersVisible = () => {
     setIsFiltersVisible(!isFiltersVisible)
+  }
+
+  const handleFilterSubmit = async () => {
+
+    const { data } = await api.get('/classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    })
+    setIsFiltersVisible(false)
+    setTeachers(data)
   }
 
   return (
@@ -31,6 +50,8 @@ export const TeacherList: React.FC = () => {
             <Label>Matéria</Label>
             <Input
               placeholder="Qual a matéria"
+              value={ subject }
+              onChangeText={ setSubject }
             />
 
             <InputGroup>
@@ -38,6 +59,8 @@ export const TeacherList: React.FC = () => {
                 <Label>Dia da semana</Label>
                 <Input
                   placeholder="Qual o dia?"
+                  value={ week_day }
+                  onChangeText={ setWeekDay }
                 />
               </InputBlock>
 
@@ -45,11 +68,13 @@ export const TeacherList: React.FC = () => {
                 <Label>Horário</Label>
                 <Input
                   placeholder="Qual horário?"
+                  value={ time }
+                  onChangeText={ setTime }
                 />
               </InputBlock>
             </InputGroup>
 
-            <SubmitButton>
+            <SubmitButton onPress={ handleFilterSubmit }>
               <SubmitButtonText>Filtrar</SubmitButtonText>
             </SubmitButton>
           </SearchForm>
@@ -58,12 +83,11 @@ export const TeacherList: React.FC = () => {
       </PageHeader>
 
       <TeacherListItems>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+
+        { teachers.map((teacher: TeacherProps) => (
+            <TeacherItem key={ teacher.id } teacher={ teacher }/>
+          ))
+        }
       </TeacherListItems>
     </Container>
   )
